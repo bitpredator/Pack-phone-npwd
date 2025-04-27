b2b547a88be13c626ca0c31f91ade34154912197
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-04-27 18:38:42 +0200

    chore: improved code formatting

--- esx-npwd/client.lua
+++ esx-npwd/client.lua
@@ -1,26 +1,28 @@
 local function getESX()
-	local ESX = exports['es_extended']:getSharedObject()
+	local ESX = exports["es_extended"]:getSharedObject()
 
 	-- Already handled internally
-	if ESX.GetConfig().OxInventory then return end
+	if ESX.GetConfig().OxInventory then
+		return
+	end
 
 	if ESX.SearchInventory then
 		return {
 			PlayerLoaded = ESX.PlayerLoaded,
-			SearchInventory = ESX.SearchInventory
+			SearchInventory = ESX.SearchInventory,
 		}
 	else
 		-- Backwards compatibility for versions prior to 1.5
 		ESX = {
 			PlayerLoaded = ESX.PlayerLoaded,
-			GetPlayerData = ESX.GetPlayerData
+			GetPlayerData = ESX.GetPlayerData,
 		}
 
 		function ESX.SearchInventory()
 			local inventory = ESX.GetPlayerData().inventory
 
 			for _, item in pairs(inventory) do
-				if item.name == 'phone' then
+				if item.name == "phone" then
 					return 1
 				end
 			end
@@ -30,41 +32,43 @@ end
 
 local ESX = getESX()
 
-if not ESX then return end
+if not ESX then
+	return
+end
 
-AddEventHandler('onClientResourceStart', function(resource)
-	if resource == 'es_extended' then
+AddEventHandler("onClientResourceStart", function(resource)
+	if resource == "es_extended" then
 		ESX = getESX()
 	end
 end)
 
 local npwd = exports.npwd
 
-if ESX.PlayerLoaded and ESX.SearchInventory('phone', true) > 0 then
+if ESX.PlayerLoaded and ESX.SearchInventory("phone", true) > 0 then
 	npwd:setPhoneDisabled(false)
 else
 	npwd:setPhoneDisabled(true)
 end
 
-RegisterNetEvent('esx:playerLoaded', function()
-	if ESX.SearchInventory('phone') > 0 then
+RegisterNetEvent("esx:playerLoaded", function()
+	if ESX.SearchInventory("phone") > 0 then
 		npwd:setPhoneDisabled(false)
 	end
 end)
 
-RegisterNetEvent('esx:onPlayerLogout', function()
+RegisterNetEvent("esx:onPlayerLogout", function()
 	npwd:setPhoneVisible(false)
 	npwd:setPhoneDisabled(true)
 end)
 
-RegisterNetEvent('esx:removeInventoryItem', function(item, count)
-	if item == 'phone' and count == 0 then
+RegisterNetEvent("esx:removeInventoryItem", function(item, count)
+	if item == "phone" and count == 0 then
 		npwd:setPhoneDisabled(true)
 	end
 end)
 
-RegisterNetEvent('esx:addInventoryItem', function(item, count)
-	if item == 'phone' then
+RegisterNetEvent("esx:addInventoryItem", function(item, count)
+	if item == "phone" then
 		npwd:setPhoneDisabled(false)
 	end
 end)
--- npwd_services/config.lua
+++ npwd_services/config.lua
@@ -4,6 +4,8 @@ Config.Jobs = {
     "mechanic",
     "cardealer",
     "taxi",
+    "police",
+    "ambulance",
 }
 
 Config.ShowOffDuty = false --show players who aren't currently on duty (QBcore Only)
